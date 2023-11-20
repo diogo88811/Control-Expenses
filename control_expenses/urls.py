2225ed170d0804d34d31aebe57171f2dd6b35c8a
diogo88811 <diogovalente00@hotmail.com>
2023-11-20 23:14:31 +0000

    create models

--- control_expenses/urls.py
+++ control_expenses/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from expenses.views import home
+from expenses.views import home, listing
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home)
+    path('home', home),
+    path('', listing)
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Transaction
 import datetime
 
 def home(request):
-    now = datetime.datetime.now()
+    date = {}
+    date['transactions'] = ['t1', 't2', 't3']
+    date['now'] = datetime.datetime.now()
     #html = "<html><body>It is now %s.</body></html>" % now
-    return render(request, 'expenses/home.html')
+    return render(request, 'expenses/home.html', date)
+
+def listing(request):
+    data = {}
+    data['transactions'] = Transaction.objects.all()
+    return render(request, 'expenses/listing.html', data)
